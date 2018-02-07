b4a59cef039f0756356f4af61631bcdaadc5d65a
v2thegreat <aliabbaszaidi@outlook.com>
2018-02-07 00:02:57 -0500

    Enhanced Code Readability

--- If Enhancer/Testing/If Enhancer.py
+++ If Enhancer/Testing/If Enhancer.py
@@ -3,11 +3,13 @@ def inline_addition(list_of_str_to_add_to,string_to_add):
 	for x in range(len(list_of_str_to_add_to)):
 		if x==len(list_of_str_to_add_to)-1:
 			s+=list_of_str_to_add_to[x]
+
 		else:
 			s+=list_of_str_to_add_to[x]+string_to_add
-	return(s)
 
-def if_enhance_and(l):
+	return s
+
+def ifEnhanceAnd(l):
 	for x in range(len(l)):
 		line=int(x)
 		x=l[x]
@@ -18,9 +20,10 @@ def if_enhance_and(l):
 			for i in range(len(t)):
 				l[line]=inline_addition(t," and ")
 			l[line ]="if"+l[line]+":"
+
 	return l
 
-def if_enhance_or(l):
+def ifEnhance(l):
 	for x in range(len(l)):
 		line=int(x)
 		x=l[x]
@@ -30,18 +33,20 @@ def if_enhance_or(l):
 				t[i]="("+t[i].strip()+")"
 				l[line]=inline_addition(t," or ")
 			l[line ]="if "+l[line]+":"
+
 	return l
 
-def if_enhance(l):
+def ifEnhance(l):
 	for x in range(len(l)):
 		if "if" in l[x]:
-			l[x]=(int(l[x].count('\t')/2))*'\t'+if_enhance_or(if_enhance_and([l[x]]))[-1]
+			l[x]=(int(l[x].count('\t')/2))*'\t'+ifEnhance(ifEnhanceAnd([l[x]]))[-1]
+
 	return l
 
 def main():
 	Code_Input_Folder="Testing_If_Enhance.py"
 	Code_Lines=open(Code_Input_Folder).read().split("\n")
-	open(Code_Input_Folder,'w').writelines([x+"\n" for x in if_enhance(Code_Lines)])
+	open(Code_Input_Folder,'w').writelines([x+"\n" for x in ifEnhance(Code_Lines)])
 
 if __name__=='__main__':
 	main()
