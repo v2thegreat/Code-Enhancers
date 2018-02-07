ee4d4205622aa8f8df860b9d9099d6ea3bba34dc
v2thegreat <aliabbaszaidi@outlook.com>
2018-02-07 01:06:01 -0500

    Refactored Unit Tests
    Refractored Unit Tests to make them easier to use

--- If Enhancer/Unit Tests/Unit Tests Code Enhancers.py
+++ If Enhancer/Unit Tests/Unit Tests Code Enhancers.py
@@ -1,34 +1,26 @@
+from ifEnhancer import *
 import unittest
 
-# def ifEnhanceAnd(l):
-# 	for x in range(len(l)):
-# 		line=int(x)
-# 		x=l[x]
-# 		if "if" in x:
-# 			t=x[x.find("if")+len("if"):-1].split("and")
-# 			for i in range(len(t)):
-# 				t[i]="("+t[i].strip()+")"
-# 			for i in range(len(t)):
-# 				l[line]=inline_addition(t," and ")
-# 			l[line ]="if"+l[line]+":"
-#
-# 	return l
+testStringOr  = ['if 5>3 or 3>4:']
+testStringAnd = ['if 5>3 and 4>3:']
+
+correctValueAnd = ['if (5>3) and (4>3):']
+correctValueOr = ['if (5>3) or (3>4):']
 
 class testingCodeEnhancer(unittest.TestCase):
 	"""docstring for testingCodeEnhancer"""
+	def setUp(self):
+		self.testStringOr  = ['if 5>3 or 3>4:']
+		self.testStringAnd = ['if 5>3 and 4>3:']
 
-	def setup(self):
-		testStringOr  = ['if 5>3 or 3>4:']
-		testStringAnd = ['if 5>3 and 4>3:']
-
-		correctValueAnd = ['if (5>3) and (4>3)']
-		correctValueOr = ['if (5>3) or (3>4):']
+		self.correctValueAnd = ['if (5>3) and (4>3):']
+		self.correctValueOr = ['if (5>3) or (3>4):']
 
 	def testingIfEnhanceAnd(self):
-		self.assertEqual(ifEnhanceAnd(testStringAnd), correctValueAnd)
+		self.assertEqual(ifEnhanceAnd(self.testStringAnd), self.correctValueAnd)
 
 	def testingIfEnhanceOr(self):
-		self.assertEqual(ifEnhanceOr(testStringOr), correctValueOr)
+		self.assertEqual(ifEnhanceOr(self.testStringOr), self.correctValueOr)
 
 if __name__ == '__main__':
     unittest.main()
